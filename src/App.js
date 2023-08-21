25948b8c66795ee1acd507517d023070a4623ff7
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-21 15:38:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React , {useState} from 'react';
+import './App.css';
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+<div className='container'>
+<h1>Self Reliance App</h1>
+</div>
+    </div>
+  );
+}
+
+export default App;
