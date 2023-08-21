92dbff5e5434d6587b2fc9c7e86931673df545fb
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-21 15:43:21 +0200

    quesitons

--- src/App.js
+++ src/App.js
@@ -14,6 +14,12 @@ function App() {
 <h2>Current Score :2</h2>
 <div className='question-card'>
 <h2>Question 1 out of 7</h2>
+<h3>I see myself as a succesfull person.</h3>
+<ul>
+  <li>never</li>
+  <li>rarely</li>
+  <li>often</li>
+</ul>
 </div>
 </div>
     </div>
