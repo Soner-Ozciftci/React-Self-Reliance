d3a48eae90bfc0b1a628677b70af4c841c359c00
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-21 16:30:06 +0200

    currentQuestion

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 
 function App() {
 
-
-
-
+const [ showFinalResult,setFinalResult]=useState(false);
+const [score,setScore]=useState(0);
+const [currentQuestion,setCurrentQuestion]= useState(0);
 
   return (
     <div className="App">
 <div className='container'>
 <h1>Self Reliance App</h1>
-<h2>Current Score :2</h2>
+<h2>Current Score :{score}</h2>
+
+{
+  showFinalResult ? 
+<div className='result'>
+  <h1>Result</h1>
+  <h2>
+    1 out of 7 self confident
+  </h2>
+  <button>Restart Test</button>
+</div>
+:
 <div className='question-card'>
 <h2>Question 1 out of 7</h2>
 <h3>I see myself as a succesfull person.</h3>
@@ -21,13 +32,10 @@ function App() {
   <li>often</li>
 </ul>
 </div>
-<div className='result'>
-  <h1>Result</h1>
-  <h2>
-    1 out of 7 self confident
-  </h2>
-  <button>Restart Test</button>
-</div>
+}
+
+
+
 </div>
     </div>
   );
